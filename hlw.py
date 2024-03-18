655593270d6e954ebbac85cd022f3c93e2aa3347
thaongo95 <thaongogia95@gmail.com>
2024-03-18 07:50:09 +0700

    hkw.py

---
+++ hlw.py
@@ -0,0 +1,13 @@
+
+def func(param1, param2, param3, param4='python'):
+	print(param1)
+	print(param2)
+	print(param3)
+	print(param4)
+	
+func("H", "T", "O", "A")
+def func1(names, num):
+    for name in names:
+        print(name)
+        print(num)
+func1(["A", "B", "C"],3)
